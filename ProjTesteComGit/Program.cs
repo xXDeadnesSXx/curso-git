9e550ac4429a5e5026106e1b261f460a3ce80fe5
Fábio Borges <oibafsegrob3@gmail.com>
2019-02-12 15:07:42 -0200

    Adicionado mensagem de Bom dia

--- ProjTesteComGit/Program.cs
+++ ProjTesteComGit/Program.cs
@@ -7,6 +7,7 @@ namespace ProjTesteComGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
